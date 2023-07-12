52054466d711a025065bcf6e0d54e4b33229d023
ismailtunc51 <tunc_tunc_51@outlook.com>
2023-07-12 11:30:02 +0300

    Tas_Kagit_Makas
    Form ile taş kağıt makas oyunu

---
+++ Tas_Kagit_Makas/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Tas_Kagit_Makas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_bilgisayar = new System.Windows.Forms.Label();
+            this.lbl_oyuncu = new System.Windows.Forms.Label();
+            this.lbl_tas = new System.Windows.Forms.Label();
+            this.lbl_kagit = new System.Windows.Forms.Label();
+            this.lbl_makas = new System.Windows.Forms.Label();
+            this.lbl_skor = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.lbl_pctas = new System.Windows.Forms.Label();
+            this.lbl_pckagit = new System.Windows.Forms.Label();
+            this.lbl_pcmakas = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_bilgisayar
+            // 
+            this.lbl_bilgisayar.AutoSize = true;
+            this.lbl_bilgisayar.Location = new System.Drawing.Point(483, 189);
+            this.lbl_bilgisayar.Name = "lbl_bilgisayar";
+            this.lbl_bilgisayar.Size = new System.Drawing.Size(51, 13);
+            this.lbl_bilgisayar.TabIndex = 0;
+            this.lbl_bilgisayar.Text = "Bilgisayar";
+            // 
+            // lbl_oyuncu
+            // 
+            this.lbl_oyuncu.AutoSize = true;
+            this.lbl_oyuncu.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbl_oyuncu.Location = new System.Drawing.Point(89, 189);
+            this.lbl_oyuncu.Name = "lbl_oyuncu";
+            this.lbl_oyuncu.Size = new System.Drawing.Size(44, 13);
+            this.lbl_oyuncu.TabIndex = 1;
+            this.lbl_oyuncu.Text = "Oyuncu";
+            // 
+            // lbl_tas
+            // 
+            this.lbl_tas.AutoSize = true;
+            this.lbl_tas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbl_tas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_tas.Location = new System.Drawing.Point(33, 244);
+            this.lbl_tas.Name = "lbl_tas";
+            this.lbl_tas.Size = new System.Drawing.Size(32, 15);
+            this.lbl_tas.TabIndex = 2;
+            this.lbl_tas.Text = "TAŞ";
+            this.lbl_tas.Click += new System.EventHandler(this.lbl_tas_Click);
+            // 
+            // lbl_kagit
+            // 
+            this.lbl_kagit.AutoSize = true;
+            this.lbl_kagit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbl_kagit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_kagit.Location = new System.Drawing.Point(94, 244);
+            this.lbl_kagit.Name = "lbl_kagit";
+            this.lbl_kagit.Size = new System.Drawing.Size(46, 15);
+            this.lbl_kagit.TabIndex = 3;
+            this.lbl_kagit.Text = "KAĞIT";
+            this.lbl_kagit.Click += new System.EventHandler(this.lbl_kagit_Click);
+            // 
+            // lbl_makas
+            // 
+            this.lbl_makas.AutoSize = true;
+            this.lbl_makas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbl_makas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_makas.Location = new System.Drawing.Point(154, 244);
+            this.lbl_makas.Name = "lbl_makas";
+            this.lbl_makas.Size = new System.Drawing.Size(53, 15);
+            this.lbl_makas.TabIndex = 4;
+            this.lbl_makas.Text = "MAKAS";
+            this.lbl_makas.Click += new System.EventHandler(this.lbl_makas_Click);
+            // 
+            // lbl_skor
+            // 
+            this.lbl_skor.AutoSize = true;
+            this.lbl_skor.BackColor = System.Drawing.Color.Red;
+            this.lbl_skor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_skor.ForeColor = System.Drawing.Color.Lime;
+            this.lbl_skor.Location = new System.Drawing.Point(231, 65);
+            this.lbl_skor.Name = "lbl_skor";
+            this.lbl_skor.Size = new System.Drawing.Size(0, 17);
+            this.lbl_skor.TabIndex = 5;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.Red;
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_start.Location = new System.Drawing.Point(234, 142);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(153, 35);
+            this.btn_start.TabIndex = 6;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // lbl_pctas
+            // 
+            this.lbl_pctas.AutoSize = true;
+            this.lbl_pctas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbl_pctas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_pctas.Location = new System.Drawing.Point(422, 244);
+            this.lbl_pctas.Name = "lbl_pctas";
+            this.lbl_pctas.Size = new System.Drawing.Size(32, 15);
+            this.lbl_pctas.TabIndex = 2;
+            this.lbl_pctas.Text = "TAŞ";
+            this.lbl_pctas.Click += new System.EventHandler(this.lbl_tas_Click);
+            // 
+            // lbl_pckagit
+            // 
+            this.lbl_pckagit.AutoSize = true;
+            this.lbl_pckagit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbl_pckagit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_pckagit.Location = new System.Drawing.Point(483, 244);
+            this.lbl_pckagit.Name = "lbl_pckagit";
+            this.lbl_pckagit.Size = new System.Drawing.Size(46, 15);
+            this.lbl_pckagit.TabIndex = 3;
+            this.lbl_pckagit.Text = "KAĞIT";
+            this.lbl_pckagit.Click += new System.EventHandler(this.lbl_kagit_Click);
+            // 
+            // lbl_pcmakas
+            // 
+            this.lbl_pcmakas.AutoSize = true;
+            this.lbl_pcmakas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbl_pcmakas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_pcmakas.Location = new System.Drawing.Point(543, 244);
+            this.lbl_pcmakas.Name = "lbl_pcmakas";
+            this.lbl_pcmakas.Size = new System.Drawing.Size(53, 15);
+            this.lbl_pcmakas.TabIndex = 4;
+            this.lbl_pcmakas.Text = "MAKAS";
+            this.lbl_pcmakas.Click += new System.EventHandler(this.lbl_makas_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(425, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(162, 148);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(438, 269);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(174, 63);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(234, 269);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(189, 63);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(35, 269);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(179, 63);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(656, 344);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.lbl_skor);
+            this.Controls.Add(this.lbl_pcmakas);
+            this.Controls.Add(this.lbl_makas);
+            this.Controls.Add(this.lbl_pckagit);
+            this.Controls.Add(this.lbl_pctas);
+            this.Controls.Add(this.lbl_kagit);
+            this.Controls.Add(this.lbl_tas);
+            this.Controls.Add(this.lbl_oyuncu);
+            this.Controls.Add(this.lbl_bilgisayar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_bilgisayar;
+        private System.Windows.Forms.Label lbl_oyuncu;
+        private System.Windows.Forms.Label lbl_tas;
+        private System.Windows.Forms.Label lbl_kagit;
+        private System.Windows.Forms.Label lbl_makas;
+        private System.Windows.Forms.Label lbl_skor;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label lbl_pctas;
+        private System.Windows.Forms.Label lbl_pckagit;
+        private System.Windows.Forms.Label lbl_pcmakas;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Tas_Kagit_Makas/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tas_Kagit_Makas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string OyuncuTercih;
+        string PcTercih;
+        int random;
+        Random rnd = new Random();
+
+        private void lbl_tas_Click(object sender, EventArgs e)
+        {
+            OyuncuTercih = "TAŞ";
+            lbl_oyuncu.Text = OyuncuTercih;
+        }
+
+        private void lbl_kagit_Click(object sender, EventArgs e)
+        {
+            OyuncuTercih = "KAĞIT";
+            lbl_oyuncu.Text = OyuncuTercih;
+        }
+
+        private void lbl_makas_Click(object sender, EventArgs e)
+        {
+            OyuncuTercih = "MAKAS";
+            lbl_oyuncu.Text = OyuncuTercih;
+        }
+        private void BilgisayarTercih()
+        {
+            random = rnd.Next(1, 4);
+            if (random == 1)
+            {
+                PcTercih = "TAŞ";
+                lbl_bilgisayar.Text = PcTercih;
+            }
+            else if (random == 2)
+            {
+                PcTercih = "KAĞIT";
+                lbl_bilgisayar.Text = PcTercih;
+            }
+            else if (random == 3)
+            {
+                PcTercih = "MAKAS";
+                lbl_bilgisayar.Text = PcTercih;
+            }
+        }
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+
+            if (OyuncuTercih == null)
+            {
+                MessageBox.Show("Hamle Şeçimi Yapmalısınız.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                BilgisayarTercih();
+            }
+
+
+            if (OyuncuTercih == "TAŞ" && PcTercih == "KAĞIT")
+            {
+                lbl_skor.Text = "BİLGİSAYAR KAZANDI";
+
+            }
+            else if (OyuncuTercih == "TAŞ" && PcTercih == "MAKAS")
+            {
+                lbl_skor.Text = "OYUNCU KAZANDI";
+            }
+            else if (OyuncuTercih == "KAĞIT" && PcTercih == "TAŞ")
+            {
+                lbl_skor.Text = "OYUNCU KAZANDI";
+            }
+            else if (OyuncuTercih == "KAĞIT" && PcTercih == "MAKAS")
+            {
+                lbl_skor.Text = "BİLGİSAYAR KAZANDI";
+
+            }
+            else if (OyuncuTercih == "MAKAS" && PcTercih == "TAŞ")
+            {
+                lbl_skor.Text = "BİLGİSAYAR KAZANDI";
+
+            }
+            else if (OyuncuTercih == "MAKAS" && PcTercih == "KAĞIT")
+            {
+                lbl_skor.Text = "OYUNCU KAZANDI";
+            }
+            else if (OyuncuTercih == PcTercih)
+            {
+                lbl_skor.Text = "KAZANAN YOK";
+            }
+
+
+        }
+
+       
+    }
+}
